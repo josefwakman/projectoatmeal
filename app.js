e98b0c1c827c1d2f35d4d1dd93b467f96ef1d27a
Josef Wakman <josef.wakman@gmail.com>
2019-01-25 16:38:39 +0100

    .

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const expressHandlebars = require("express-handlebars")
+
+const app = express()
+
+// -----------
+
+app.engine('hbs', expressHandlebars({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
+
+
+// Vi använder routers istället för det här sen va?
+// --- GET Requests ----------
+
+app.get('/', function(req, res) {
+    res.render('home.hbs')
+})
+
+
+
+app.listen(8080)
