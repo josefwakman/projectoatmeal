c598b7937622ae093d5ada2884f4dd53eb3341bc
josefwakman <josef.wakman@gmail.com>
2019-01-29 16:02:57 +0100

    add book search

--- app.js
+++ app.js
@@ -10,6 +10,46 @@ app.engine('hbs', expressHandlebars({
     extname: '.hbs'
 }))
 
+// ---- PLACEHOLDERS - TO BE DELETED ----------------
+
+
+
+books = [
+    {
+        title: "Yo",
+        author: "Mr. X"
+    },
+    {
+        title: "Woop Woop",
+        author: "Mr. X"
+    },
+    {
+        title: "Harry Potter and the Somewhat Unengaged Chemistry Proffesor",
+        author: "Dandy MacBloom"
+    },
+    {
+        title: "A Night at the Chicago opera House",
+        author: "Andy Beepmaster"
+    },
+    {
+        title: "Harry Potter and Another Test Title",
+        author: "DandyMacBloom"
+    },
+    {
+        title: "Du bist eine Kartoffel, Harry",
+        author: "M. Bartoscheck"
+    }
+]
+
+bookTitles = [
+    "Yo", 
+    "Woop Woop",
+    "Harry Potter and the Somewhat Unengaged Chemistry Proffesor",
+    "A Night at the Chicago opera House",
+    "Harry Potter and Another Test Title",
+    "Du bist eine Kartoffel, Harry",
+]
+
 
 // Vi använder routers istället för det här sen va?
 // --- GET Requests ----------
@@ -19,7 +59,23 @@ app.get('/', function(req, res) {
 })
 
 app.get('/search-books', function(req, res) { 
-    res.render("search-books.hbs")
+    model = {searched: false}
+
+    if (0 < Object.keys(req.query).length) {
+
+        lowerCaseSearch = req.query.search.toLowerCase()
+        const foundBooks = bookTitles.filter(function(str) {
+            lowerCaseStr = str.toLowerCase()
+            return lowerCaseStr.search(lowerCaseSearch) > -1
+        })
+
+        model = {
+            searched: true,
+            books: foundBooks
+        }
+    }
+
+    res.render("search-books.hbs", model)
 })
 
 app.get('/search-authors', function(req, res) {
