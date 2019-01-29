5df2697fa878c98c33fbb235477c1baf0b83ea06
josefwakman <josef.wakman@gmail.com>
2019-01-29 14:35:57 +0100

    add pages for links

--- app.js
+++ app.js
@@ -14,8 +14,24 @@ app.engine('hbs', expressHandlebars({
 // Vi använder routers istället för det här sen va?
 // --- GET Requests ----------
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
     res.render("search-books.hbs")
+})
+
+app.get('/search-books', function(req, res) { 
+    res.render("search-books.hbs")
+})
+
+app.get('/search-authors', function(req, res) {
+    res.render("search-authors.hbs")
+})
+
+app.get('/administrators', function(req, res) {
+    res.render("administrators.hbs")
+})
+
+app.get('/login', function(req, res) {
+    res.render("login.hbs")
 })
 
 
