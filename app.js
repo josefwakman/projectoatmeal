b42f09ed27a418dad59261125494edd4be7469d6
Erik Strand <erikstrand00@gmail.com>
2019-01-29 14:47:15 +0100

    Added bootstrap functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const expressHandlebars = require("express-handlebars")
 
 const app = express()
 
+const path = require('path')
+
 // -----------
 
 app.engine('hbs', expressHandlebars({
@@ -10,6 +12,7 @@ app.engine('hbs', expressHandlebars({
     extname: '.hbs'
 }))
 
+app.use(express.static(path.join(__dirname, '/public')))
 
 // Vi använder routers istället för det här sen va?
 // --- GET Requests ----------
